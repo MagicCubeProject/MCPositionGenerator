f1d82949e84673331a9a6690c64338fcfbd549e0
narekmouse <narek.mouse@gmail.com>
2018-05-12 15:27:47 +0400

    add runner

---
+++ runner.py
@@ -0,0 +1,13 @@
+from GenerationController.GenControl import GControl
+
+
+def main():
+    g = GControl("/work/MagicCubeLib/db")
+    g.start_fisrt_gen()
+    g.start_second_gen()
+    g.start_third_gen()
+    g.start_4_gen()
+    g.start_5_gen()
+
+if __name__ == "__main__":
+    main()
