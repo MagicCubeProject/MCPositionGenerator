3d67f25d35c3874d1d2cb370340b367d072bdca9
narekmouse <narek.mouse@gmail.com>
2018-06-17 13:13:25 +0400

    Add New Runner

--- runner.py
+++ runner.py
@@ -1,13 +1,28 @@
 #from GenerationController.GenControl import GControl
+from telegram.ext import Updater
 from GenerationController.GenerationController import GenerationController
-
+import telegram
 
 def main():
+    bot = telegram.Bot(token='557166714:AAFxqnpOvzgpN9q1_8ATJIK2d_qxioTgOjM')
+    updater = Updater(token='557166714:AAFxqnpOvzgpN9q1_8ATJIK2d_qxioTgOjM')
+    updates = bot.get_updates(limit=2)
+    chat_id = updates[0].message.chat._id_attrs[0]
+    print(bot.get_me())
+    bot.send_message(chat_id=chat_id, text="\n\n----Start---\n")
+    g = GenerationController("/work/MagicCubeLib/db","TestDB004")
+    for x in range(25):
+        msg = "Starting Make Generation : {gen}"
+        bot.send_message(chat_id=chat_id,
+                            text=msg.format(gen=x)
+                         )
+        size =g.start_gen(x)
+        msg1 = "Elemt count of Generation : {gen} | is : {size}"
+        bot.send_message(chat_id=chat_id,
+                            text=msg1.format(gen=x,size=size)
+                         )
+    bot.send_message(chat_id=chat_id, text="\n\n----Done---\n")
 
-
-    g = GenerationController("/work/MagicCubeLib/db","TestDB002")
-    for x in range(5):
-        g.start_gen(x)
 
 
 if __name__ == "__main__":
